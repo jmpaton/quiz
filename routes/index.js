fbca3afd2f22a2400399a2e511f590dc76e10288
jmpaton <jesus.paton@gmail.com>
2015-08-04 09:39:15 +0200

    Quiz 16 - Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,10 @@ router.get('/', function(req, res) {
 
 // Autoload
 router.param('quizId', quizController.load);
+
+router.get('/login',								sessionController.new);
+router.post('/login',								sessionController.create);
+router.get('/logout',								sessionController.destroy);
 
 // Rutas /quizes
 router.get('/quizes', 								quizController.index);
@@ -23,7 +28,7 @@ router.put('/quizes/:quizId(\\d+)', 				quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 				quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', 	commentController.create);
+router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
 
 router.get('/author', function (req, res) { res.render('author', {errors: []}); });
 
